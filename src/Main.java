fe877a187a279a6a9699e1504728a9c4d91be3dd
Granttt <35479977+Granttt@users.noreply.github.com>
2018-09-17 12:29:24 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World 高希阳!");
+        System.out.println("Hello World 高希阳123!");
         System.out.print("sdfsd");
 
     }
